b09996463e882d250c1850494603d72a379f55f6
mariasanguesa <msanguesa3@gmail.com>
2023-03-10 16:01:23 +0100

    Improve header

--- src/UI/Header.js
+++ src/UI/Header.js
@@ -1,23 +1,30 @@
-import Nav from 'react-bootstrap/Nav';
 import './Header.css';
-import {Link} from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {Person, Bag} from 'react-bootstrap-icons';
 
 function Header() {
     return (
-        <div className='header'>
-        <Nav>
-            <Nav>
-                <Link className='inicio' to='/'>Inicio</Link> | {"    "}
-            </Nav>
-            <Nav>
-                <Link className='tienda' to='/tienda'>Tienda</Link> | {"        "}
-            </Nav>
-            <Nav>
-                <Link className='acerca' to='/acerca'>Acerca de </Link> | {"        "}
-            </Nav>
-        </Nav>
-
-    </div>
+        <>
+            <div variant="d-flex">
+                <Navbar style={{ 'height': '70px' }}>
+                    <Container >
+                        <Navbar.Brand href="/"><b>BARRAS GO</b></Navbar.Brand>
+                        <Nav>
+                            <Nav.Link href="/">INICIO</Nav.Link>
+                            <Nav.Link href="/tienda">TIENDA</Nav.Link>
+                            <Nav.Link href="/acerca">ACERCA DE</Nav.Link>
+                            <Nav.Link><Person/></Nav.Link>
+                            <Nav.Link><Bag/></Nav.Link>
+                        </Nav>
+                    </Container>
+                </Navbar>
+                <Navbar bg='dark' variant="dark justify-content-center align-items-center" style={{ 'height': '40px' }}>
+                    <p style={{ 'color': 'white' }}>Envios gratis con pedidos superiores a 30 euros</p>
+                </Navbar>
+            </div>
+        </>
     )
 }
 export default Header;
