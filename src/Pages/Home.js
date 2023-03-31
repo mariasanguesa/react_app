0e546aa31b670f52566efd382e07e52a1160c197
mariasanguesa <msanguesa3@gmail.com>
2023-03-31 10:32:46 +0200

    Add searcher

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,34 +5,34 @@ import img from "../images/slide1.png";
 import img2 from "../images/slide2.png";
 import img3 from "../images/slide3.png";
 
-const Home = (props) => {
+const Home = () => {
     return (
-      <Container>
+      <Container className="container-md">
         <Row>
-        <Carousel fade style={{maxHeight:"370px"}}>
+        <Carousel style={{maxHeight:"440px"}} interval={2000}>
         <Carousel.Item>
-        <div className="d-flex justify-content-sm-between">
-          <div className="p-2"><Image src={img} style={{marginLeft:"250px"}}/> </div>
-          <div className="p-2" style={{marginRight:"100px", color:"#24be5a"}}><h3 style={{marginTop:"80px"}}>Pide 2 cajas y</h3>
+        <div className="d-flex justify-content-md-between">
+          <div className="p-2"><Image src={img} width="450px"/></div>
+          <div className="p-2" style={{marginRight:"20%",color:"#24be5a"}}><h3 style={{marginTop:"12%"}}>Pide 2 cajas y</h3>
                    <h1 style={{fontWeight:"bold"}}>AHORRA UN 20%</h1>
-                   <Button href="/tienda" variant="dark" style={{marginTop:"20px"}}>Comprar Ahora</Button></div>
+                   <Button href="/tienda" variant="dark" style={{marginTop:"5%"}}>Comprar Ahora</Button></div>
         </div>
           
         </Carousel.Item>
         <Carousel.Item>
-        <div className="d-flex justify-content-sm-between">
-          <div className="p-2"><Image  className="img-fluid" src={img2} style={{marginLeft:"250px"}}/></div>
-          <div className="p-2" style={{marginRight:"150px", color:"#dfdf04"}}><h3 style={{marginTop:"80px"}}>Prueba el</h3>
+        <div className="d-flex justify-content-md-between">
+          <div className="p-2"><Image src={img2}  width="450px"/></div>
+          <div className="p-2" style={{marginRight:"25%",color:"#dfdf04"}}><h3 style={{marginTop:"12%"}}>Prueba el</h3>
                    <h1 style={{fontWeight:"bold"}}>NUEVO SABOR</h1>
-                   <Button href="/tienda" variant="dark" style={{marginTop:"20px"}}>Comprar Ahora</Button></div>
+                   <Button href="/tienda" variant="dark" style={{marginTop:"5%"}}>Comprar Ahora</Button></div>
         </div>
         </Carousel.Item>
         <Carousel.Item>
-        <div className="d-flex justify-content-sm-between" style={{maxHeight:"360px"}}>
-          <div className="p-2"><Image src={img3} style={{marginLeft:"250px"}}/></div>
-          <div className="p-2" style={{marginRight:"230px", color:"#d30079"}}><h3 style={{marginTop:"80px"}}>COME BIEN</h3>
-                   <h1 style={{fontWeight:"bold"}}>VIVE BIEN</h1>
-                   <Button href="/tienda" variant="dark" style={{marginTop:"20px"}}>Comprar Ahora</Button></div>
+        <div className="d-flex justify-content-md-between" style={{maxHeight:"435px"}}>
+          <div className="p-2"><Image src={img3}  width="450px"/></div>
+          <div className="p-2" style={{marginRight:"33%", color:"#d30079"}}><h3 style={{marginTop:"17%"}}>¡COME BIEN!</h3>
+                   <h1 style={{fontWeight:"bold"}}>¡VIVE BIEN!</h1>
+                   <Button href="/tienda" variant="dark" style={{marginTop:"8%"}}>Comprar Ahora</Button></div>
         </div>
         </Carousel.Item>
       </Carousel>
--- src/Pages/Tienda.js
+++ src/Pages/Tienda.js
@@ -1,37 +1,96 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
 import ProductoHome from "../Components/ProductoHome";
-
+import { useState } from 'react';
+import '../App.css';
+import { Search } from 'react-bootstrap-icons';
+import Col from 'react-bootstrap/Col';
 
 const Tienda = (props) => {
+
     let contenido = "";
 
-    if (Object.keys(props.productosTienda).length > 1) {
-        for (let i = 0; i < Object.keys(props.productosTienda).length; i++) {
+    const [busqueda, setBusqueda] = useState("");
+    const [productosFiltrados, setProductosFiltrados] = useState([]);
+
+
+    const handleChange = (e) => {
+        setBusqueda(e.target.value);
+        filtrar(e.target.value);
+    }
+
+    const filtrar = (terminoBusqueda) => {
+        let arrayFiltrados = [];
+        props.productosTienda.filter((elemento) => {
+            if (elemento.nombre.toUpperCase().includes(terminoBusqueda.toUpperCase())
+            ) {
+                arrayFiltrados.push({
+                    filtro: elemento
+                })
+            }
+            setProductosFiltrados(arrayFiltrados);
+        });
+    }
+
+    if (productosFiltrados.length > 1) {
+        contenido = <Row>{
+            productosFiltrados.map((elemento) => {
+                return (
+                    <ProductoHome loginData={props.loginData} productos={props.productos} key={elemento.filtro.nombre} nombre={elemento.filtro.nombre} foto={elemento.filtro.foto} precio={elemento.filtro.precio} producto={elemento.filtro.id} añadirCarrito={props.añadirCarrito} />
+                )
+            })
+        }
+        </Row>
+
+    } else {
+        if (busqueda) {
+            contenido = <div className="text-center align-items-center justify-content-center mt-5 mb-5">¡Lo sentimos! No hay coincidencias con tu búsqueda.</div>;
+        } else {
             contenido = <Row>{
-                Object.values(props.productosTienda).map((elemento) => {
+                props.productosTienda.map((elemento) => {
                     return (
                         <ProductoHome loginData={props.loginData} productos={props.productos} key={elemento.nombre} nombre={elemento.nombre} foto={elemento.foto} precio={elemento.precio} producto={elemento.id} añadirCarrito={props.añadirCarrito} />
                     )
                 })
-            }</Row>
+            }
+            </Row>
         }
 
-    };
+    }
 
 
 
     return (
         <>
-            <p style={{ fontWeight: "bold", fontSize: "30px", textAlign: "center", marginTop: "10px" }}>LOS PRODUCTOS</p>
+            <p style={{ fontWeight: "bold", fontSize: "30px", textAlign: "center", marginTop: "15px" }}>LOS PRODUCTOS</p>
+
+            <div className="App">
+                <Container className="text-center align-items-center justify-content-center mt-4 mb-4">
+                    <Row className="justify-content-md-center">
+                        <Col md="auto" lg="5">
+                            <input
+                                className="form-control inputBuscar"
+                                value={busqueda}
+                                placeholder="Búsqueda por nombre de producto"
+                                onChange={handleChange}
+                            />
+                        </Col>
+                        <Col xs lg="1">
+                            <Search />
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
             <Container>
                 <Row>
                     {contenido}
                 </Row>
             </Container>
-
         </>
+
     )
+
 }
+
 
 export default Tienda;
